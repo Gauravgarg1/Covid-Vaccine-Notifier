527ce1477f9c0ac51af5d45e8960120072eaa2f8
Gaurav Garg <garggaurav628@gmail.com>
2021-07-01 00:22:44 +0530

    update

--- vaccine.js
+++ vaccine.js
@@ -6,6 +6,8 @@ let pincode = process.argv[2];
 let age = process.argv[3];
 //username input to send details on whatsapp
 let personName = process.argv[4];
+//vaccine you want
+let vaccineyouwant=process.argv[5];
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
@@ -23,7 +25,7 @@ pdfDoc.pipe(fs.createWriteStream('Vaccine.pdf'));
         args: ["--start-maximized"]
     });
     //calling the function to get all the details
-    let slots = await getlistofplaces(links, browser, pincode);
+    let slots = await getlistofplaces(links, browser, pincode,vaccineyouwant);
 
 
     //Here after getting the details we will send the information to user on whatsapp
@@ -35,23 +37,40 @@ pdfDoc.pipe(fs.createWriteStream('Vaccine.pdf'));
         await page.waitForSelector(".G8bNp", { visible: true });
     }, 5000);
 
-    console.log(12);
+    // console.log(12);
     await page.waitForSelector(".RPX_m", { visible: true });
     await page.click(".RPX_m");
     await page.type(".RPX_m", personName);
     await page.keyboard.press("Enter", { delay: 200 });
     await page.waitForSelector("._2A8P4");
     await page.click("._2A8P4");
+      // message
+      let message="";
+    if( Object.keys(slots).length==0)
+    {
+        message="Hello Sir, there are NO available slots of "+ vaccineyouwant;
+        await page.type("._2A8P4", message);
+        await page.keyboard.press("Enter", { delay: 200 });
+    }
+  
+    else
+    {
+     message="Hello Sir, these are the available slots of "+ vaccineyouwant;
+     await page.type("._2A8P4", message);
+     await page.keyboard.press("Enter", { delay: 200 });
     await page.type("._2A8P4", JSON.stringify(slots));
     await page.keyboard.press("Enter", { delay: 200 });
-    console.log(13);
+    // console.log(13);
+    }
 
 
 
     //Here i added additionall functionality for those who run any organisation 
     // so they can take a printout of it and paste on the notice board and notifies to all the society members
     let details = JSON.stringify(slots, null, 2);
+    pdfDoc.text(message);
     pdfDoc.text(details);
+    
     pdfDoc.end();
 
 
@@ -63,7 +82,7 @@ pdfDoc.pipe(fs.createWriteStream('Vaccine.pdf'));
 
 //function to get the listing of Vaccine Available
 
-async function getlistofplaces(link, browserInstance, pincode) {
+async function getlistofplaces(link, browserInstance, pincode,vaccineyouwant) {
     let newPage = await browserInstance.newPage();
     await newPage.goto(link);
     await newPage.waitForSelector("#mat-input-0", { visible: true });
@@ -78,28 +97,38 @@ async function getlistofplaces(link, browserInstance, pincode) {
 
     else {
 
-        await newPage.click(".col-padding.filerandsearchblock.margin0auto [for='flexRadioDefault2']");
+        await newPage.click(".col-padding.filerandsearchblock.margin0auto [for='flexRadioDefault12']");
     }
+
+    if(vaccineyouwant=="Covishield")
+    await newPage.click(".mobile-hide [for='flexRadioDefault4'].form-check-label");
+    else
+    await newPage.click(".mobile-hide [for='flexRadioDefault5'].form-check-label");
 
     let today = new Date();
     let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+    // let date=document.querySelector(".availability-date p").innerText;
     let cost = "Free";
 
     function consoleFn(date, place, no_vaccine, type) {
         let places = document.querySelectorAll(place);
         let vaccines = document.querySelectorAll(no_vaccine);
-        let t = document.querySelectorAll(type);
         let details = [];
-        for (let i = 0, j = 0; i < places.length && j < vaccines.length; i++, j += 7) {
-            if (vaccines[j].innerText == "Booked") {
-                continue;
-            }
+        let j=-7;
+        for (let i = 0;  i < places.length ; i++) {
+
+            j+=7;
+            
             let Available_at = places[i].innerText;
             let No_of_Vaccines = vaccines[j].innerText;
-            let type_of_Vaccine = t[j].innerText;
+            if(No_of_Vaccines=="NA")
+            continue;
+            let type_of_Vaccine = type;
             details.push({
                 date, Available_at, No_of_Vaccines, type_of_Vaccine
             })
+        
 
         }
 
@@ -107,6 +136,6 @@ async function getlistofplaces(link, browserInstance, pincode) {
         return details;
     }
     return await newPage.evaluate(consoleFn, date,
-        ".row-disp>.center-name-text", ".vaccine-box.vaccine-box1.vaccine-padding a", ".vaccine-box.vaccine-box1.vaccine-padding h5");
+        ".row-disp>.center-name-text", ".vaccine-box.vaccine-box1.vaccine-padding a", vaccineyouwant);
 }
 
